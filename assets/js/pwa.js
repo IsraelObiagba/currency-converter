055b466af38d023b5135d4b9ee6d0579c63ae8b6
Israel Obiagba <iobiagba@outlook.com>
2018-07-01 22:05:46 +0100

    pwa edits

---
+++ assets/js/pwa.js
@@ -0,0 +1,13 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("https://israelobiagba.github.io/currency-converter/assets/js/sw.js", { scope: "" })
+    .then(function() {
+      console.log("Congratulations!!Service Worker Registered");
+    })
+    .catch(function(err) {
+      console.log(`Aagh! Some kind of Error :- ${err}`);
+    });
+} else {
+  //still not supported
+  console.log("Sorry, not supported!")
+}
