d1545c408c9ffe0ab69da376b5ee24aefbc6682d
IsraelObiagba <iobiagba@outlook.com>
2018-07-01 23:53:52 +0100

    added idb.js

--- assets/js/pwa.js
+++ assets/js/pwa.js
@@ -1,13 +1,13 @@
-if ("serviceWorker" in navigator) {
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register("https://israelobiagba.github.io/currency-converter/assets/js/sw.js", { scope: "" })
-    .then(function() {
-      console.log("Congratulations!!Service Worker Registered");
+    .register('sw.js')
+    .then(() => {
+      console.log('Congratulations!!Service Worker Registered')
     })
-    .catch(function(err) {
-      console.log(`Aagh! Some kind of Error :- ${err}`);
-    });
+    .catch(function (err) {
+      console.log(`Aagh! Some kind of Error :- ${err}`)
+    })
 } else {
-  //still not supported
-  console.log("Sorry, not supported!")
+  // still not supported
+  console.log("Sorry, your browser doesn't support it!")
 }
